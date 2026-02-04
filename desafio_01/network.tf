f409412b23c5cc39f07cc2b47cb23057bb917937
hiali <hialidias@gmail.com>
2026-02-04 14:02:19 -0300

    feat: adicionar subnets

--- desafio_01/network.tf
+++ desafio_01/network.tf
@@ -1,1 +1,47 @@
-module "network"
+resource "aws_vpc" "main" {
+  cidr_block = var.base_cidr_block
+
+  tags = {
+   Name = "vpc-main"
+  }
+}
+
+resource "aws_subnet" "public_1" {
+   vpc_id = aws_vpc.main.id
+   cidr_block = var.public_subnet_cidrs[0]
+   availability_zone = var.availability_zones[0]
+
+   tas = {
+      Name = "public-subnet-1"
+   }
+}
+
+resourse "aws_subnet" "public_2" {
+   vpc_id = aws_vpc.main.id
+   cidr_block = var.public_subnet_cidrs[1]
+   availability_zone = var.availability_zones[1]
+
+   tags = {
+      Name = "public-vpc-2"
+   }
+}
+
+resourse "aws_subnet" "private_1" {
+   vpc_id = aws_vpc.main.id
+   cidr_block = var.private_subnet_cidrs[0]
+   availability_zone = var.availability_zones[0]
+
+   tags = {
+      Name = "private-vpc-1"
+   }
+}
+
+resourse "aws_subnet" "private_2" {
+   vpc_id = aws_vpc.main.id
+   cidr_block = var.private_subnet_cidrs[1]
+   availability_zone = var.availability_zones[1]
+
+   tags = {
+      Name = "private-vpc-2"
+   }
+}
--- desafio_01/variables.tf
+++ desafio_01/variables.tf
@@ -1,25 +1,23 @@
-VPC: Crie uma VPC com o CIDR 10.0.0.0/16.
-
-Multi-AZ: Use duas Availability Zones (ex: us-east-1a e us-east-1b).
-
-Subnets:
-
-2 Subnets Públicas (uma em cada AZ).
-
-2 Subnets Privadas (uma em cada AZ).
-
 variable "base_cidr_block" {
    description = "The base CIDR block for the VPC"
    type = string
-   defaut = "10.0.0.0/16"
+   default = "10.0.0.0/16"
 }
 
-variable "availability_zone" {
+variable "availability_zones" {
    description = "A list of availability zones in which ro create subnets"
    type = list(string)
    default = ["us-east-1a", "us-east-1b"]
 }
 
-variable "public_subnet"{
-   
+variable "public_subnet_cidrs" {
+   description = "A list of CIDR blocks for the public subnets"
+   type = list(string)
+   default = ["10.0.1.0/16", "10.0.2.0/16"]
+}
+
+variable "private_subnet_cidrs" {
+   description = "Alist of CIDR blocks for the private subnets"
+   type = list(string)
+   default = ["10.0.3.0/16", "10.0.4.0/16"]
 }
