c4196b586fc3aa9bc73d1f40c15dd6f9d7eaf152
hiali <hialidias@gmail.com>
2026-02-07 16:16:10 -0300

    feat: add internet gateway

--- desafio_01/network.tf
+++ desafio_01/network.tf
@@ -11,12 +11,12 @@ resource "aws_subnet" "public_1" {
    cidr_block = var.public_subnet_cidrs[0]
    availability_zone = var.availability_zones[0]
 
-   tas = {
+   tags = {
       Name = "public-subnet-1"
    }
 }
 
-resourse "aws_subnet" "public_2" {
+resource "aws_subnet" "public_2" {
    vpc_id = aws_vpc.main.id
    cidr_block = var.public_subnet_cidrs[1]
    availability_zone = var.availability_zones[1]
@@ -26,7 +26,7 @@ resourse "aws_subnet" "public_2" {
    }
 }
 
-resourse "aws_subnet" "private_1" {
+resource "aws_subnet" "private_1" {
    vpc_id = aws_vpc.main.id
    cidr_block = var.private_subnet_cidrs[0]
    availability_zone = var.availability_zones[0]
@@ -36,12 +36,20 @@ resourse "aws_subnet" "private_1" {
    }
 }
 
-resourse "aws_subnet" "private_2" {
+resource "aws_subnet" "private_2" {
    vpc_id = aws_vpc.main.id
    cidr_block = var.private_subnet_cidrs[1]
    availability_zone = var.availability_zones[1]
 
    tags = {
       Name = "private-vpc-2"
+   }
+}
+
+resource "aws_internet_gateway" "igw" {
+   vpc_id = aws_vpc.main.id
+
+   tags = {
+      Name = "main-igw"
    }
 }
