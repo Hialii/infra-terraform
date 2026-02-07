f1cc61c4f089cbf89335232efeccd095cb5133e1
hiali <hialidias@gmail.com>
2026-02-07 17:40:12 -0300

    feat: add nat gateway

--- desafio_01/network.tf
+++ desafio_01/network.tf
@@ -53,3 +53,64 @@ resource "aws_internet_gateway" "igw" {
       Name = "main-igw"
    }
 }
+
+resource "aws_route_table" "public_route_table" {
+   vpc_id = aws_vpc.main.id
+
+   route {
+      cidr_block = "0.0.0.0/0"
+      gateway_id = aws_internet_gateway.igw.id
+   }
+
+   tags = {
+      Name = "route-table-main"
+   }
+}
+
+resource "aws_route_table_association" "public_1" {
+   subnet_id = aws_subnet.public_1.id
+   route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_route_table_association" "public_2" {
+   subnet_id = aws_subnet.public_2.id
+   route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_eip" "nat_eip" {
+   doamin = "vpc"
+}
+
+resource "aws_nat_gateway" "ngw" {
+   allocation_id = aws_eip.nat_eip.id
+   subnet_id = aws_subnet.public_1.id
+
+   tags = {
+      Name = "main_ngw"
+   }
+
+   depends_on = [aws_internet_gateway.igw]
+}
+
+resource "aws_route_table" "private_route_table" {
+   vpc_id = aws_vpc.main.id
+
+   route {
+      cidr_block = "0.0.0.0/0"
+      nat_gateway_id = aws_nat_gateway.ngw.id
+   }
+
+   tags = {
+      Name = "private-route-table"
+   }
+}
+
+resource "aws_route_table_association" "private_1" {
+   subnet_id = aws_subnet.private_1.id
+   route_table_id = aws_route_table.private_route_table.id
+}
+
+resource "aws_route_table_association" "private_2" {
+   subnet_id = aws_subnet.private_2.id
+   route_table_id = aws_route_table.private_route_table.id
+}
