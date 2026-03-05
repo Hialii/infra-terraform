7c7238d58189fb4698949a781a537b10cc401df8
hiali <hialidias@gmail.com>
2026-03-05 16:30:21 -0300

    feat: add ingress rule to 22 port

--- desafio_01/security.tf
+++ desafio_01/security.tf
@@ -13,6 +13,15 @@ resource "aws_vpc_security_group_ingress_rule" "sg_web_ingress" {
    ip_protocol = "tcp"
 }
 
+resource "aws_vpc_security_group_ingress_rule" "sg_web_ingress_access" {
+   security_group_id = aws_security_group.sg_web.id
+
+   cidr_ipv4 = "0.0.0.0/0"
+   from_port = 22
+   to_port = 22
+   ip_protocol = "tcp"
+}
+
 resource "aws_security_group" "sg_bd" {
    name ="sg_bd"
    description = "Security group for main VPC"
@@ -23,6 +32,14 @@ resource "aws_vpc_security_group_ingress_rule" "sg_bd_ingress" {
    security_group_id = aws_security_group.sg_bd.id
    from_port = 5432
    to_port = 5432
+   ip_protocol = "tcp"
+   referenced_security_group_id = aws_security_group.sg_web.id 
+}
+
+resource "aws_vpc_security_group_ingress_rule" "sg_bd_ingress_access" {
+   security_group_id = aws_security_group.sg_bd.id
+   from_port = 22
+   to_port = 22
    ip_protocol = "tcp"
    referenced_security_group_id = aws_security_group.sg_web.id 
 }
