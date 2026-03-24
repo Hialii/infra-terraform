3aa24d6cfbbc695dae8aee0626a4148d83cd0cfe
hiali <hialidias@gmail.com>
2026-03-24 16:55:54 -0300

    feat: add output

--- desafio_01/outputs.tf
+++ desafio_01/outputs.tf
@@ -2,4 +2,16 @@ output "vpc_id" {
    value = aws_vpc.main.id
 }
 
+output "public_ec2_ip" {
+   value = aws_instance.ec2_public_1.public_ip
+}
+
+output "private_ec2_id" {
+   value = aws_instance.ec2_private_1.private_ip
+}
+
+
+
+
+
 
